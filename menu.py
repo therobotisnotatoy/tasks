0a9f193400a7680172abc8822d4477fb85149f88
therobotisnotatoy <pythonshumor@gmail.com>
2022-04-25 19:16:10 +0300

    Bugfix: tasks without arguments were intercepted by try/except block

--- menu.py
+++ menu.py
@@ -1,28 +1,30 @@
 """Menu implementation"""
 from inspect import signature
 
-from tasks import task_87, task_226, task_559
+from tasks import (task_87, task_226, task_559, task_test)
 
 
 def main():
-    """Menu implementation"""
-
+    """
+    Menu implementation
+    """
     # Dictionary of tasks. Key - task number, value - function of that task
     tasks_dict = {
         "Enter 'E' to exit": exit,
         "87": task_87,
         "226": task_226,
         "559": task_559,
+        "T": task_test,
     }
 
     # Main loop
     while True:
 
         # Print available tasks
-        print("-" * 60)
+        print("*" * 24, "MAIN MENU", "*" * 25)
         print("Available tasks: ")
         print(", ".join(list(tasks_dict.keys())))
-        print("-" * 60)
+        print("*" * 60)
 
         try:
             task_number = input("Input task`s number: ").upper()
@@ -41,27 +43,31 @@ def main():
         # Task loop
         while True:
             try:
-                # Show module info & get arguments (or "exit" key)
-                print("-" * 60)
-                user_input = input(func.info()).upper()
-
-                # Exit from task loop
-                if user_input == "R":
-                    break
-
-                # Get arguments (you validation function could be here)
-                args = [int(arg) for arg in user_input.split(" ") if (arg.isnumeric() and (int(arg) > 0))]
-
                 # Get number of arguments needed for task run() function
                 expected_args_count = len(signature(func.run).parameters)
+                if expected_args_count < 1:
+                    # Show module info & call function without arguments
+                    print("-" * 60)
+                    print(func.info())
+                    print("-" * 60)
+                    print("Result: ", func.run())
+                    break
+                else:
+                    # Show module info & get arguments (or "exit" key)
+                    print("-" * 60)
+                    user_input = input(func.info()).upper()
+                    # Exit from task loop
+                    if user_input == "R":
+                        break
+                    # Get arguments (you validation function could be here)
+                    args = [int(arg) for arg in user_input.split(" ") if (arg.isnumeric() and (int(arg) > 0))]
+                    # Validate number of parameters
+                    assert(expected_args_count == len(args))
 
-                # Validate number of parameters
-                assert(expected_args_count == len(args))
+                    print("-" * 60)
 
-                print("-" * 60)
-
-                # Get the result
-                print("Result: ", func.run(*args))
+                    # Get the result
+                    print("Result: ", func.run(*args))
 
             except (TypeError, AssertionError):
                 print("-" * 60)
